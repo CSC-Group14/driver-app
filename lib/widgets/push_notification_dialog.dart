f3504f973d2f54f186735e31e9768dc338acda33
katsronnie <katenderonnie045@gmai.com>
2024-08-11 16:40:37 +0300

    i have added some logics in trip screen

--- lib/widgets/push_notification_dialog.dart
+++ lib/widgets/push_notification_dialog.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+<<<<<<< HEAD
+import 'package:logitrust_drivers/mainScreens/new_trip_screen.dart';
+import 'package:logitrust_drivers/models/riderequest.dart';
+=======
 import 'package:logitrust_drivers/models/riderequest.dart';
 import 'package:logitrust_drivers/mainScreens/new_trip_screen.dart';
+>>>>>>> d31fbabd0115e5b914d3b7c5fcd07a7e1aa6b712
 
 class RideRequestDialog extends StatefulWidget {
   final String rideRequestId;
@@ -13,14 +18,186 @@ class RideRequestDialog extends StatefulWidget {
 }
 
 class _RideRequestDialogState extends State<RideRequestDialog> {
+<<<<<<< HEAD
+  late DatabaseReference _rideRequestRef;
+  RideRequest? _rideRequest;
+=======
   final DatabaseReference _rideRequestRef =
       FirebaseDatabase.instance.ref().child('AllRideRequests');
       late Stream<DatabaseEvent> _rideRequestsStream;
   late RideRequest request;
+>>>>>>> d31fbabd0115e5b914d3b7c5fcd07a7e1aa6b712
 
   @override
   void initState() {
     super.initState();
+<<<<<<< HEAD
+    _rideRequestRef = FirebaseDatabase.instance
+        .ref()
+        .child("AllRideRequests")
+        .child(widget.rideRequestId);
+
+    _fetchRideRequestData();
+  }
+
+  Future<void> _fetchRideRequestData() async {
+    try {
+      final event = await _rideRequestRef.once();
+      if (event.snapshot.value != null) {
+        final data = event.snapshot.value as Map<dynamic, dynamic>;
+        final rideRequest = RideRequest.fromMap(data);
+
+        setState(() {
+          _rideRequest = rideRequest;
+        });
+      } else {
+        setState(() {
+          _rideRequest = null; // or show an appropriate message
+        });
+      }
+    } catch (error) {
+      print("Error fetching ride request data: $error");
+    }
+  }
+
+  void _redirectToNewTripScreen() {
+    if (_rideRequest == null) return;
+
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => NewTripScreen(
+          rideRequest: _rideRequest!,
+        ),
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Ride request accepted.')),
+    );
+  }
+
+  Future<void> _declineRideRequest() async {
+    try {
+      await _rideRequestRef.update({
+        'status': 'Declined',
+      });
+
+      Navigator.of(context).pop(); // Close the dialog
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Ride request declined.')),
+      );
+    } catch (error) {
+      print("Error declining ride request: $error");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("New Ride Request"),
+      content: _rideRequest == null
+          ? CircularProgressIndicator()
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Pickup Details
+                  Row(
+                    children: [
+                      Image.asset(
+                        'images/source.png',
+                        width: 25,
+                        height: 25,
+                      ),
+                      const SizedBox(width: 22),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            "Source Address: ${_rideRequest!.sourceAddress}",
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  // Destination Details
+                  Row(
+                    children: [
+                      Image.asset(
+                        'images/destination.png',
+                        width: 25,
+                        height: 25,
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Container(
+                          child: Text(
+                            "Destination Address: ${_rideRequest!.destinationAddress}",
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+      actions: [
+        TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.blue,
+          ),
+          onPressed: _redirectToNewTripScreen,
+          child: const Text(
+            "Accept",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.red,
+          ),
+          onPressed: _declineRideRequest,
+          child: const Text(
+            "Decline",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.green,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text(
+            "Cancel",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+=======
     _fetchRideRequestData();
     _rideRequestsStream = _rideRequestRef.onValue;
   }
@@ -41,6 +218,7 @@ class _RideRequestDialogState extends State<RideRequestDialog> {
       print("Error fetching ride request data: $error");
       Navigator.of(context).pop(); // Close the dialog if there's an error
     });
+>>>>>>> d31fbabd0115e5b914d3b7c5fcd07a7e1aa6b712
   }
 
   void _acceptRide() async {
@@ -211,4 +389,14 @@ void showRideRequestDialog(BuildContext context, String rideRequestId) async {
       SnackBar(content: Text('Ride request not found.')),
     );
   }
+}
+
+// Function to show the RideRequestDialog
+void showRideRequestDialog(BuildContext context, String rideRequestId) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return RideRequestDialog(rideRequestId: rideRequestId);
+    },
+  );
 }

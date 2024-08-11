d31fbabd0115e5b914d3b7c5fcd07a7e1aa6b712
katsronnie <katenderonnie045@gmai.com>
2024-08-11 14:19:47 +0300

    i have added the routes

--- lib/widgets/push_notification_dialog.dart
+++ lib/widgets/push_notification_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:logitrust_drivers/models/riderequest.dart';
+import 'package:logitrust_drivers/mainScreens/new_trip_screen.dart';
 
 class RideRequestDialog extends StatefulWidget {
   final String rideRequestId;
@@ -12,98 +13,77 @@ class RideRequestDialog extends StatefulWidget {
 }
 
 class _RideRequestDialogState extends State<RideRequestDialog> {
-  late DatabaseReference _rideRequestRef;
-  RideRequest? _rideRequest;
+  final DatabaseReference _rideRequestRef =
+      FirebaseDatabase.instance.ref().child('AllRideRequests');
+      late Stream<DatabaseEvent> _rideRequestsStream;
+  late RideRequest request;
 
   @override
   void initState() {
     super.initState();
-    _rideRequestRef = FirebaseDatabase.instance
-        .ref()
-        .child("AllRideRequests")
-        .child(widget.rideRequestId);
-
     _fetchRideRequestData();
+    _rideRequestsStream = _rideRequestRef.onValue;
   }
 
   void _fetchRideRequestData() {
-    _rideRequestRef.once().then((DatabaseEvent event) {
+    _rideRequestRef.child(widget.rideRequestId).once().then((DatabaseEvent event) {
       if (event.snapshot.value != null) {
         final data = event.snapshot.value as Map<dynamic, dynamic>;
         final rideRequest = RideRequest.fromMap(data);
 
-        if (rideRequest.status == 'Pending') {
-          setState(() {
-            _rideRequest = rideRequest;
-          });
-        } else {
-          // Close the dialog if the status is not pending
-          Navigator.of(context).pop();
-        }
-      } else {
-        // Handle case when data does not exist
         setState(() {
-          _rideRequest = null; // or show an appropriate message
+          request = rideRequest;
         });
+      } else {
+        Navigator.of(context).pop(); // Close the dialog if no data
       }
     }).catchError((error) {
       print("Error fetching ride request data: $error");
+      Navigator.of(context).pop(); // Close the dialog if there's an error
     });
   }
 
-  void _acceptRideRequest() {
-    _rideRequestRef.once().then((DatabaseEvent event) {
-      if (event.snapshot.value != null) {
-        final data = event.snapshot.value as Map<dynamic, dynamic>;
-        final rideRequest = RideRequest.fromMap(data);
+  void _acceptRide() async {
+    if (request != null) {
+      try {
+        await _rideRequestRef.child(request!.id).update({
+          'status': 'Accepted',
+        });
 
-        if (rideRequest.status == 'Pending') {
-          // Update the status to accepted
-          _rideRequestRef.update({
-            'status': 'Accepted',
-            'acceptedBy': 'DriverID', // Replace 'DriverID' with the actual driver ID
-          }).then((_) {
-            Navigator.of(context).pop(); // Close the dialog
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Ride request accepted.')),
-            );
-          }).catchError((error) {
-            print("Error accepting ride request: $error");
-          });
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('This ride request has already been handled.')),
-          );
-          Navigator.of(context).pop(); // Close the dialog
-        }
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Ride request not found.')),
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NewTripScreen(
+              rideRequest: request!,
+            ),
+          ),
         );
-        Navigator.of(context).pop(); // Close the dialog
+      } catch (e) {
+        print('Error updating ride status: $e');
       }
-    }).catchError((error) {
-      print("Error fetching ride request data: $error");
-    });
+    }
   }
 
   void _declineRideRequest() {
-    _rideRequestRef.update({'status': 'Declined'}).then((_) {
-      Navigator.of(context).pop(); // Close the dialog
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Ride request declined.')),
-      );
-    }).catchError((error) {
-      print("Error declining ride request: $error");
-    });
+    if (request != null) {
+      _rideRequestRef.child(request!.id).update({'status': 'Declined'}).then((_) {
+        Navigator.of(context).pop(); // Close the dialog
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Ride request declined.')),
+        );
+      }).catchError((error) {
+        print("Error declining ride request: $error");
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    
     return AlertDialog(
       title: Text("New Ride Request"),
-      content: _rideRequest == null
-          ? CircularProgressIndicator()
+      content: request == null
+          ? Text("No ride request data available.") // Optional
           : SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -120,7 +100,7 @@ class _RideRequestDialogState extends State<RideRequestDialog> {
                       Expanded(
                         child: Container(
                           child: Text(
-                            "Source Address: ${_rideRequest!.sourceAddress}",
+                            "Source Address: ${request!.sourceAddress}",
                             style: const TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
@@ -144,7 +124,7 @@ class _RideRequestDialogState extends State<RideRequestDialog> {
                       Expanded(
                         child: Container(
                           child: Text(
-                            "Destination Address: ${_rideRequest!.destinationAddress}",
+                            "Destination Address: ${request!.destinationAddress}",
                             style: const TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
@@ -163,7 +143,7 @@ class _RideRequestDialogState extends State<RideRequestDialog> {
           style: TextButton.styleFrom(
             backgroundColor: Colors.blue,
           ),
-          onPressed: _acceptRideRequest,
+          onPressed: _acceptRide,
           child: const Text(
             "Accept",
             style: TextStyle(
@@ -208,11 +188,27 @@ class _RideRequestDialogState extends State<RideRequestDialog> {
   }
 }
 
-void showRideRequestDialog(BuildContext context, String rideRequestId) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      return RideRequestDialog(rideRequestId: rideRequestId);
-    },
-  );
+bool _dialogShowing = false;
+
+void showRideRequestDialog(BuildContext context, String rideRequestId) async {
+  if (_dialogShowing) return;
+
+  final rideRequestRef = FirebaseDatabase.instance.ref().child('AllRideRequests').child(rideRequestId);
+  final snapshot = await rideRequestRef.once();
+
+  if (snapshot.snapshot.value != null) {
+    _dialogShowing = true;
+    showDialog(
+      context: context,
+      builder: (context) {
+        return RideRequestDialog(rideRequestId: rideRequestId);
+      },
+    ).then((_) {
+      _dialogShowing = false; // Reset the flag when the dialog is closed
+    });
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Ride request not found.')),
+    );
+  }
 }
